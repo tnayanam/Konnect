607673487e9702d1d8daaeea4ce5911ee93f4212
unknown <tnayanam@syr.edu>
2018-03-21 14:30:01 -0400

    Initial Project Commit

---
+++ SyrasoftConnect/DAL/DBAccess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+   public class DBAccess
+    {
+        public static void GetDataFromDB()
+        {
+            //var CS = @"Data Source=.\;Initial Catalog=Connect;Integrated Security=SSPI";// ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString.ToString();
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter("Select * from tblTenant; Select * from tblUnit;", con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+            }
+        } 
+    }
+}
